3ea1dcf0671f9cf0523c40dd743debf33ec15ba9
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-26 23:05:23 +0545

    register page css

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -3,44 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register Page</title>
     <link rel="stylesheet" href="{{asset('user/user.css')}}">
 </head>
 <body>
     <div class="all">
-    <div class="img">
- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp366E1uTL9ry1MAhpDQ2uAxj94E-xYYb4sRgi6vBpqU6gJsjCs38WvWQh3YGEl-0xZZo&usqp=CAU" alt="">
-    </div>
-    <div class="form">
-    <!-- @if(Session::has('success'))
-        <div class="alert alert-success">
-            {{Session::get('success')}}
+            <div class="img">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp366E1uTL9ry1MAhpDQ2uAxj94E-xYYb4sRgi6vBpqU6gJsjCs38WvWQh3YGEl-0xZZo&usqp=CAU" alt="">
+            </div>
+            <div class="form">
+            <!-- @if(Session::has('success'))
+                <div class="alert alert-success">
+                    {{Session::get('success')}}
+                </div>
+            @endif
+            @if(Session::has('fail'))
+                <div class="alert alert-danger">
+                    {{Session::get('fail')}}
+                </div>
+            @endif -->
+
+            <h1 class="page-title">Create a New Account</h1>
+            <form action="{{asset('')}}" method="POST">
+                @csrf
+                <label for="name">Full Name:</label>
+                <input type="text" name="name" id="name" required>
+
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" required>
+
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" required>
+
+                <label for="password">Conform Password:</label>
+                <input type="password" name="cpassword" id="password" required>
+
+                <button type="submit">Register</button>
+            </form>
         </div>
-    @endif
-    @if(Session::has('fail'))
-        <div class="alert alert-danger">
-            {{Session::get('fail')}}
-        </div>
-    @endif -->
-
-    <label for="name">Create a new account</label>
-    <form action="{{asset('')}}" method="POST">
-        @csrf
-        <label for="name">Full Name:</label>
-        <input type="text" name="name" id="name" required>
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required>
-
-        <label for="password">Conform Password:</label>
-        <input type="password" name="cpassword" id="password" required>
-
-        <button type="submit">Register</button>
-    </form>
-</div>
     </div>
     
 </body>
