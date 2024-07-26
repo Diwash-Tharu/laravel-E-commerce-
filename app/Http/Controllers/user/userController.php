736af920e4c15e4b3e0a63cd6f35babc3219862b
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-26 23:46:47 +0545

    register wth the dada base

--- app/Http/Controllers/user/userController.php
+++ app/Http/Controllers/user/userController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class userController extends Controller
 {
- public function registerPageShowUp(){
-     return view('user.register');
- }
+    public function registerPageShowUp()
+    {
+        return view('user.register');
+    }
+
+    public function registerUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return redirect()->route('user.login')->with('success', 'User created successfully');
+    }
 }
